44c7f936bcb467811d76c805a7c6bf84a035be3c
mtae616 <mtae616@naver.com>
2022-04-19 18:01:44 +0900

    init doublylist

--- list/DoublyLinkedList/doublylist.c
+++ list/DoublyLinkedList/doublylist.c
@@ -2,7 +2,11 @@
 
 DoublyList* createDoublyList()
 {
+	DoublyList *buf;
+
+	buf = (DoublyList *)calloc(1, sizeof(DoublyList));
 	
+	return (buf);
 }
 
 void deleteDoublyList(DoublyList* pList)
@@ -27,15 +31,62 @@ void clearDoublyList(DoublyList* pList)
 
 int getDoublyListLength(DoublyList* pList)
 {
-
+	return (pList->currentElementCount);
 }
 
 DoublyListNode* getDLElement(DoublyList* pList, int position)
 {
+	DoublyListNode	*buf;
 
+	buf = pList->headerNode.pRLink;
+	while (position--) {
+		buf = buf->pRLink;
+	}
+
+	return(buf);
 }
 
 void displayDoublyList(DoublyList* pList)
 {
+	int				i;
+	DoublyListNode	*buf;
 
+	i = -1;
+	buf = pList->headerNode.pRLink;
+	while (++i <= pList->currentElementCount) {
+		printf("pList[%d] = %d",i, buf->data);
+		buf = buf->pRLink;
+	}
+}
+
+int main()
+{
+	DoublyList	*temp;
+	DoublyListNode a;
+	DoublyListNode b;
+	DoublyListNode c;
+	DoublyListNode *d;
+
+	temp = createDoublyList();
+
+	
+
+	a.data = 1;
+	a.pLLink = NULL;
+	a.pRLink = &b;
+
+	temp->headerNode.pRLink = &a;
+	
+	b.data = 2;
+	b.pLLink = &a;
+	b.pRLink = &c;
+
+	c.data = 3;
+	c.pLLink = &b;
+	c.pRLink = NULL;
+
+	d = malloc(sizeof(DoublyListNode));
+	d = getDLElement(temp, 2);
+	printf("%d", d->data);
+	
 }
