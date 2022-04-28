c3dc6cf77fccc4db43cc01ad47462b465f53f489
Taesu Kang <mtae616@naver.com>
2022-04-28 17:06:55 +0900

    add stack/maze comment

--- stack/maze/maze.c
+++ stack/maze/maze.c
@@ -1,6 +1,6 @@
 #include "mapdef.h"
 
-void showPath(LinkedStack *pStack)
+void showPath(LinkedStack *pStack) // 유효한 경로 출력
 {
     int temp[HEIGHT][WIDTH] = {0, };
     StackNode   *buf;
@@ -9,16 +9,15 @@ void showPath(LinkedStack *pStack)
     for (int i = pStack->currentElementCount; i > 0; i--)
     {
         buf = popLS(pStack);
-
         next = buf->pLink;
-        temp[buf->data.y][buf->data.x] = 2;
+        temp[buf->data.y][buf->data.x] = 2; // stack 에 담긴 값을 토대로 지나온 길을 표시
         free(buf);
         buf = next;
     }
     printMaze(temp);
 }
 
-void    stack_swap(LinkedStack *pStack)
+void    stack_swap(LinkedStack *pStack) // 거꾸로 담겨있는 stack을 순서대로 새로운 stack에 할당
 {
     LinkedStack *new_stack = createLinkedStack();
     StackNode   *temp;
@@ -36,20 +35,20 @@ void    stack_swap(LinkedStack *pStack)
     deleteLinkedStack(new_stack);
 }
 
-int is_valid(int x, int y)
+int is_valid(int x, int y) // 좌표는 0 이상, WIDTH, HEIGHT 미만이어야 한다.
 {
     return (x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT);
 }
 
-void findPath(int mazeArray[HEIGHT][WIDTH], MapPosition startPos, MapPosition endPos, LinkedStack *pStack)
+void findPath(int mazeArray[HEIGHT][WIDTH], MapPosition startPos, MapPosition endPos, LinkedStack *pStack) // 경로 찾기
 {
-    int         exit = FALSE;
-    int         marked_map[HEIGHT][WIDTH] = {0, };
+    int         exit = FALSE; // 종료 조건
+    int         marked_map[HEIGHT][WIDTH] = {0, }; // memoization
     StackNode   *temp;
     MapPosition buf;
     int         dir = 0, x = 0, y = 0;
 
-    pushLSMapPosition(pStack, startPos);
+    pushLSMapPosition(pStack, startPos); // start node
     marked_map[startPos.x][startPos.y] = TRUE;
     while(!exit)
     {
@@ -59,19 +58,19 @@ void findPath(int mazeArray[HEIGHT][WIDTH], MapPosition startPos, MapPosition en
         dir = temp->data.direction;
         while(dir < 4 && !exit)
         {
-            int new_y = y + DIRECTION_OFFSETS[dir][1];
+            int new_y = y + DIRECTION_OFFSETS[dir][1]; // 다음 경로 판별
             int new_x = x + DIRECTION_OFFSETS[dir][0];
-            if (new_y == endPos.y && new_x == endPos.x)
+            if (new_y == endPos.y && new_x == endPos.x) // 종료 조건
             {
                 pushLSMapPosition(pStack, makeNode(x, y, dir));
                 pushLSMapPosition(pStack, makeNode(new_x, new_y, dir));
                 exit = TRUE;
             }
-            else if(mazeArray[new_y][new_x] == FALSE && marked_map[new_y][new_x] == FALSE && is_valid(new_x, new_y))
+            else if(mazeArray[new_y][new_x] == FALSE && marked_map[new_y][new_x] == FALSE && is_valid(new_x, new_y)) // 판별
             {
                 marked_map[new_y][new_x] = TRUE;
                 pushLSMapPosition(pStack, makeNode(x, y, dir));
-                x = new_x;
+                x = new_x; // 값 갱신
                 y = new_y;
                 dir = 0;
             }
@@ -83,7 +82,7 @@ void findPath(int mazeArray[HEIGHT][WIDTH], MapPosition startPos, MapPosition en
     stack_swap(pStack);
 }
 
-MapPosition makeNode(int x, int y, int dir)
+MapPosition makeNode(int x, int y, int dir) // node 안에 들어갈 mapposition
 {
     MapPosition buf;
 
@@ -93,7 +92,7 @@ MapPosition makeNode(int x, int y, int dir)
     return (buf);
 }
 
-int pushLSMapPosition(LinkedStack *pStack, MapPosition node)
+int pushLSMapPosition(LinkedStack *pStack, MapPosition node) // linked Stack
 {
     StackNode   *new_one = calloc(1, sizeof(StackNode));
 
@@ -106,7 +105,7 @@ int pushLSMapPosition(LinkedStack *pStack, MapPosition node)
     return (TRUE);
 }
 
-void printMaze(int mazeArray[HEIGHT][WIDTH])
+void printMaze(int mazeArray[HEIGHT][WIDTH]) // printing
 {
     for (int i = 0; i < HEIGHT; i++)
     {
