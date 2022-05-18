2156fe6023866490d2113298be2e15cebc9e314d
강태수 <mtae616@naver.com>
2022-05-18 19:37:14 +0900

    add tree/BST comments

--- tree/BST/bst.c
+++ tree/BST/bst.c
@@ -1,6 +1,6 @@
 #include "bst.h"
 
-BinSearchTree   *makeBinSearchTree(BinSearchTreeNode rootNode)
+BinSearchTree   *makeBinSearchTree(BinSearchTreeNode rootNode) // 생성
 {
     BinSearchTree   *temp = calloc(1, sizeof(BinSearchTree));
     temp->pRootNode = calloc(1, sizeof(BinSearchTreeNode));
@@ -15,14 +15,14 @@ BinSearchTreeNode   *getRootNodeBST(BinSearchTree *pBinSearchTree)
     return (pBinSearchTree->pRootNode);
 }
 
-void    deleteBinSearchTree(BinSearchTree *pBinSearchTree)
+void    deleteBinSearchTree(BinSearchTree *pBinSearchTree) // 트리 삭제
 {
     deleteBinSearchTreeNode(pBinSearchTree->pRootNode);
     free(pBinSearchTree);
     pBinSearchTree = NULL;
 }
 
-void    deleteBinSearchTreeNode(BinSearchTreeNode *pNode)
+void    deleteBinSearchTreeNode(BinSearchTreeNode *pNode) // 개별 노드 삭제
 {
     if (pNode)
     {
@@ -33,35 +33,35 @@ void    deleteBinSearchTreeNode(BinSearchTreeNode *pNode)
     }
 }
 
-BinSearchTreeNode   *insertBST(BinSearchTree *pBinSearchTree, BinSearchTreeNode element)
+BinSearchTreeNode   *insertBST(BinSearchTree *pBinSearchTree, BinSearchTreeNode element) // node 삽입
 {
     BinSearchTreeNode   *buf;
-    BinSearchTreeNode   *new_one = calloc(1, sizeof(BinSearchTreeNode));
+    BinSearchTreeNode   *new_one = calloc(1, sizeof(BinSearchTreeNode)); 
 
-    *new_one = element;
+    *new_one = element; // 새로 할당할 node
     new_one->pRightChild = 0;
     new_one->pLeftChild = 0;
-    buf = pBinSearchTree->pRootNode;
+    buf = pBinSearchTree->pRootNode; // root노드를 시작으로 들어갈 위치의 부모노드를 저장한다.
     while(1)
     {
-        if (buf->key > new_one->key)
+        if (buf->key > new_one->key) // 새로들어갈 노드가 더 작다면
         {
-            if (!(buf->pLeftChild))
+            if (!(buf->pLeftChild)) // 부모노드의 left child가 없다면
             {
-                buf->pLeftChild = new_one;
+                buf->pLeftChild = new_one; // 부모 노드와 이어준다.
                 return (new_one);
             }
-            else
+            else // 내려가 다시 탐색한다.
                 buf = buf->pLeftChild;
         }
-        else if (buf->key < new_one->key)
+        else if (buf->key < new_one->key) // 새로들어갈 노드가 더 크다면
         {
-            if(!(buf->pRightChild))
+            if(!(buf->pRightChild)) // 부모노드의 right child가 없다면
             {
-                buf->pRightChild = new_one;
+                buf->pRightChild = new_one; // 부모 노드와 이어준다.
                 return (new_one);
             }
-            else
+            else // 내려가 탐색한다.
                 buf = buf->pRightChild;
         }
         else
@@ -69,48 +69,48 @@ BinSearchTreeNode   *insertBST(BinSearchTree *pBinSearchTree, BinSearchTreeNode 
     }
 }
 
-BinSearchTreeNode *LorR(BinSearchTreeNode *temp, int searchKey)
+BinSearchTreeNode *LorR(BinSearchTreeNode *temp, int searchKey) // 왼쪽, 오른쪽 자식 노드인지 판별
 {
     BinSearchTreeNode   *buf;
 
     buf = NULL;
-    if (searchKey == temp->pRightChild->key)
+    if (searchKey == temp->pRightChild->key) // 만약 오른쪽 자식 노드라면
     {
-        buf = temp->pRightChild;
+        buf = temp->pRightChild; // 오른쪽 자식으로 할당한다.
         temp->pRightChild = NULL;
-        if (buf->pLeftChild && !(buf->pRightChild)) // 자식 노드 1개
+        if (buf->pLeftChild && !(buf->pRightChild)) // 자식 노드 1개때 부모 노드와의 연결을 끊는다.
             temp->pRightChild = buf->pLeftChild;
-        else if (!(buf->pLeftChild) && buf->pRightChild) // 자식 노드 1개
+        else if (!(buf->pLeftChild) && buf->pRightChild) 
             temp->pRightChild = buf->pRightChild;
     }
-    else if (searchKey == temp->pLeftChild->key)
+    else if (searchKey == temp->pLeftChild->key) // 만약 왼쪽 자식 노드라면
     {
-        buf = temp->pLeftChild;
+        buf = temp->pLeftChild; // 왼쪽 자식 노드로 할당한다.
         temp->pLeftChild = NULL;
-        if (buf->pLeftChild && !(buf->pRightChild)) // 자식 노드 1개
+        if (buf->pLeftChild && !(buf->pRightChild)) // 자식 노드 1개때 부모 노드와의 연결을 끊는다.
             temp->pLeftChild = buf->pLeftChild;
-        else if (!(buf->pLeftChild) && buf->pRightChild) // 자식 노드 1개
+        else if (!(buf->pLeftChild) && buf->pRightChild) 
             temp->pLeftChild = buf->pRightChild;
     }
     return (buf);
 }
 
-void    deleteBST(BinSearchTree *pBinSearchTree, int searchKey)
+void    deleteBST(BinSearchTree *pBinSearchTree, int searchKey) // 개별 노드 삭제
 {
-    BinSearchTreeNode   *temp, *delNode, *pSuccessor, *pPredecessor;
+    BinSearchTreeNode   *temp, *delNode, *pSuccessor, *pPredecessor; // 각각 삭제할 노드의 부모노드, 삭제할 노드, 계승할 노드, 계승할 노드의 부모노드 이다.
 
-    temp = pBinSearchTree->pRootNode;
+    temp = pBinSearchTree->pRootNode; // 삭제할 노드의 부모노드로, Root로 시작한다.
     delNode = NULL;
     while (1)
     {
-        if (temp->key == searchKey)
+        if (temp->key == searchKey) // 만약 삭제할 노드가 root라면 delNode를 temp로 할당한다.
             delNode = temp;
         if ((temp->pRightChild && temp->pRightChild->key == searchKey) || (temp->pLeftChild && temp->pLeftChild->key == searchKey))
-        {
-            delNode = LorR(temp, searchKey);
+        { // 자식 중 delNode가 있다면,
+            delNode = LorR(temp, searchKey); // 함수로 들어가 왼쪽인지 오른쪽인지 판별한다.
             break ;
         }
-        if (temp->pLeftChild && temp->key > searchKey)
+        if (temp->pLeftChild && temp->key > searchKey) // 자식 중 searchKey에 해당하지 않으면 다시 탐색한다.
             temp = temp->pLeftChild;
         else if (temp->pRightChild && temp->key < searchKey)
             temp = temp->pRightChild;
@@ -119,28 +119,28 @@ void    deleteBST(BinSearchTree *pBinSearchTree, int searchKey)
     }
     if (!delNode)
         return ;
-    else if (delNode->pLeftChild && delNode->pRightChild) // 자식노드 2
+    else if (delNode->pLeftChild && delNode->pRightChild) // 자식노드 2개라면,
     {
-        pSuccessor = delNode->pLeftChild;
+        pSuccessor = delNode->pLeftChild; // 삭제할 노드의 왼쪽으로 간 뒤 맨 오른쪽 노드가 계승노드로 둔다.
         while(pSuccessor->pRightChild)
         {
             pPredecessor = pSuccessor;
             pSuccessor = pSuccessor->pRightChild;
         }
-        if (pSuccessor->pLeftChild)
+        if (pSuccessor->pLeftChild) // 만약 계승노드에 자식노드 (무조건 왼쪽이다.)가 있다면 부모노드와 이어준다.
             pPredecessor->pRightChild = pSuccessor->pLeftChild;
-        else
+        else // 계승노드에 자식노드가 없다면 연결을 끊어준다.
             pPredecessor->pRightChild = NULL;
-        if (pBinSearchTree->pRootNode->key == searchKey)
-            pBinSearchTree->pRootNode = pSuccessor;
-        else if (temp->key < pSuccessor->key)
+        if (pBinSearchTree->pRootNode->key == searchKey) // 만약 삭제할 노드가 root 였다면
+            pBinSearchTree->pRootNode = pSuccessor; // root를 갱신단다.
+        else if (temp->key < pSuccessor->key) // 삭제한 노드의 부모노드와 이어준다.
             temp->pRightChild = pSuccessor;
-        else if (temp->key > pSuccessor->key)
+        else if (temp->key > pSuccessor->key) // 삭제한 노드의 부모노드와 이어준다.
             temp->pLeftChild = pSuccessor;
-        pSuccessor->pLeftChild = delNode->pLeftChild;
+        pSuccessor->pLeftChild = delNode->pLeftChild; // 삭제한 노드의 자식노드와 계승노드를 이어준다.
         pSuccessor->pRightChild = delNode->pRightChild;
     }
-    free(delNode); // 자식노드 0, 1
+    free(delNode); // 할당 해제, 자식노드가 0개일 때는 고려할 것 없이 바로 할당해제 한다.
     delNode = NULL;
 }
 
